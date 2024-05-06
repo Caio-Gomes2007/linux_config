ba7c865ddf04470c4e210e654447ae5defb9a97a
Caio Gomes <caiofgomesrj@gmail.com>
2024-05-06 12:05:59 -0300

    fatoração do codigo

--- config_linux.bash
+++ config_linux.bash
@@ -1,83 +1,31 @@
-#! /bin/sh
+#!/bin/bash
 echo 'inicio'
 
-# atualizando oS.o
+programa=('code' 'discord' 'github-desktop' 'gnome-shell-pomodoro' 'nodejs' 'npm' 'gparted')
+
+# atualizando o S.O.
 sudo apt upgrade -y
 
-# atualizando repositorio
+# atualizando o repositório
 sudo apt update -y
 
-#instalando programas base
-echo 'instalndo o code'
-sudo apt install code -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o github'
-sudo apt install github-desktop -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o pomodoro'
-sudo apt-get install gnome-shell-pomodoro -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o nodejs'
-sudo apt install nodejs -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o npm'
-sudo apt install npm -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o gparted'
-sudo apt install gparted -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
-echo 'instalndo o discord'
-sudo apt install discord -y
-if [ $? -eq 0 ]; then
-    echo "Instalação bem-sucedida."
-    # Coloque aqui os comandos que deseja executar se a instalação for bem-sucedida
-else
-    echo "Erro durante a instalação. O script será interrompido."
-    exit 1  # Encerra o script com código de saída 1 (ou qualquer outro código de erro)
-fi
+# instalando programas base
+for i in ${!programa[@]}; do
+    echo "Instalando ${programa[$i]}"
+    sudo apt install -y ${programa[$i]}
+    if [ $? -eq 0 ]; then
+        echo "Instalação bem-sucedida."
+    else
+        echo "Erro durante a instalação. O script será interrompido."
+        exit 1
+    fi
+done
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
 sudo apt autoremove -y
-sudo reboot
+## sudo reboot
 # ---------------------------------------------------------------------- #
 echo 'final'
-
